42670a164b62a9bb16f7ad11f4cf818afd1aa870
bhardwaj94 <bhardwaj.info2cs@gmail.com>
2018-11-08 18:42:04 +0530

    First build commit

---
+++ src/components/about/about.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+export default class About extends Component{
+    render() {
+      return (
+        <section id="about">
+          <div className="row">
+            <div className="three columns">
+              <img className="profile-pic" src="images/profilepic.jpg" alt=""/>
+            </div>
+            <div className="nine columns main-col">
+              <h2>About Me</h2>
+              <p>I've always had a passion for technology and product 
+                development - even before becoming a developer, having 
+                worn many hats in my career between business development, 
+                sales and product management for tech giants and startups
+               in China, Brazil and the US. I use my experience to be more 
+               than just a coder, connecting business requirements with modern technology.<br />
+
+              I have 2+ years of development experience building fullstack 
+              products from scratch, developing and integrating APIs, working with a
+              variety of databases.
+
+              </p>
+              <div className="row">
+                <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="address">
+                    <span>Bhardwaj Chauhdary</span><br />
+                    <span>(+91)-9082012401</span><br />
+                    <span>bhardwaj.info2cs@gmail.com</span>
+                  </p>
+                </div>
+                <div className="columns download">
+                  <p>
+                    <a href="#" className="button"><i className="fa fa-download" />Download Resume</a>
+                  </p>
+                </div>
+              </div> 
+            </div> 
+          </div>
+        </section>
+      );
+    }
+  };
---
+++ src/components/resume/resumeData.js
@@ -0,0 +1,7 @@
+let resumeData = {
+    "imagebaseurl":"http://bhardwaj94.github.io/",
+    "name": "Bhardwaj Chaudhary",
+    "role": "Fullstack web developer",
+    "tech":"React JS | JavaScript | Node JS"
+    }
+  export default resumeData
